0780197c3e5105afd9b4cfeb59c7d99e6503bc35
ryllevisto2020 <ryllevisto13@gmail.com>
2026-05-16 15:35:01 +0800

    Visto

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\touchstarClient;
+use App\Models\touchstarClientAccount;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class ClientController extends Controller
 {
     //
     public function addClient(Request $req){
-        dd($req);
+        $profilePic = null;
+
+        if($req->file("client_profilepic") != null){
+            $profilePic = $req->file("client_profilepic")->store("ClientProfilePic");
+        }
+
+        $collection = collect($req);
+        $data = $collection->except(["client_profilepic"]);
+
+        $data->put("client_profilepic",$profilePic);
+
+        touchstarClient::create($data->all())->save();
+
+        return Response()->json(["status"=> "success"]);
+    }
+    public function addAccount(Request $req){
+        touchstarClientAccount::create([
+            "client_id" => $req->client_id,
+            "client_email" => $req->client_email,
+            "client_password" => Hash::make($req->client_password),
+        ])->save();
+
+        touchstarClient::find($req->client_id)->update([
+            "client_account"=>"TRUE"
+        ]);
+        return Response()->json(["status"=> "success"]);
     }
 }
--- app/Models/touchstarClientAccount.php
+++ app/Models/touchstarClientAccount.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class touchstarClientAccount extends Model
 {
     //
+    public $table = "touchstarclientaccount";
+    public $timestamps = false;
+    public $fillable = [
+        "client_id",
+        "client_email",
+        "client_password",
+        "client_login_status"
+    ];
+    public $hidden = [
+        "client_password"
+    ];
+    public $primaryKey = "client_account_id";
 }
